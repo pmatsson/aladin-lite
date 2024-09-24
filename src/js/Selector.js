b778ce380a1ebabbbd896d502b5350546389db30
Philip Matsson <philip@winterway.eu>
2024-09-24 17:33:32 +0200

    Push overlayItems in selector getObjects as an array for consistency

--- src/js/Selector.js
+++ src/js/Selector.js
@@ -147,7 +147,7 @@ export class Selector {
                     }
 
                     if (o.intersectsBBox(x, y, w, h, view)) {
-                        objList.push(o);
+                        objList.push([o]);
                     }
                 }
             }
